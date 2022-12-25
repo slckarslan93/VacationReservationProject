914ea0872ea56ee28639cf75fd5986c08f1f1fb1
Selcuk ARSLAN <slckarslan93@gmail.com>
2022-12-25 16:23:18 +0300

    Generic Repository

--- EntityLayer/Concrete/MainFeature.cs
+++ EntityLayer/Concrete/MainFeature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace EntityLayer.Concrete
 {
     public class MainFeature
     {
+        [Key]
         public int FeatureId { get; set; }
+
         public string Title { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
--- EntityLayer/Concrete/Testimonial.cs
+++ EntityLayer/Concrete/Testimonial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace EntityLayer.Concrete
 {
     public class Testimonial
     {
+        [Key]
         public int TestimonialId { get; set; }
+
         public string Client { get; set; }
         public string Comment { get; set; }
         public string ClientImage { get; set; }
